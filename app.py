df6904ff09c4b52052c14ddf7903eaeaa458e510
Droplet <darellpetualang@gmail.com>
2024-03-05 11:18:44 +0700

    send message payload

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import requests
 
 url = "https://gate.whapi.cloud/messages/text?token=haTYU7n4fIryOWHkAqUsqz0xKHsOg1Oh"
-paylaod = {
-    "typing_time": 5,
-    "to": "+628386"
+
+payload = {
+    "typing_time": 2,
+    "to": "62895392349444@s.whatsapp.net",
+    "body": "Pesan ini dikirim melalui python dengan whapi, flask."
 }
+
+headers = {
+    "accept":"application/json",
+    "content-type":"application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.text)
